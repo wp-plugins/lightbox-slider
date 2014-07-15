e1e54075977ba36b70201eea58d0e4eaa4da8907
weblizar <weblizar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-15 20:59:16 +0000

    commit
    git-svn-id: https://plugins.svn.wordpress.org/lightbox-slider/trunk@949073 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/pro-aid-slider/script.js
@@ -0,0 +1,18 @@
+
+/*============================================
+	iPad Slider
+	==============================================*/
+	jQuery('.ipad-slider').flexslider({
+		prevText: '<i class="fa fa-angle-left"></i>',
+		nextText: '<i class="fa fa-angle-right"></i>',
+		animation: 'slide',
+		slideshowSpeed: 3000,
+		useCSS: true,
+		controlNav: false,
+		pauseOnAction: false, 
+		pauseOnHover: true,
+		smoothHeight: false
+	});
+
+
+	

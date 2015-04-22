a370fe5e370041e120e1f387c8453da9d04a52ec
weblizar <weblizar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-22 05:37:07 +0000

    commit version 1.8
    git-svn-id: https://plugins.svn.wordpress.org/lightbox-slider/trunk@1141704 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lightbox-slider.php
+++ lightbox-slider.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Lightbox Slider
- * Version: 1.7
+ * Version: 1.8
  * Description: A Great Lightbox plugin to create and display various types of images galleries on WordPress blog.
  * Author: Weblizar
  * Author URI: http://www.weblizar.com
